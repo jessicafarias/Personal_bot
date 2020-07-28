b498afccd271ed454a6e405275db4a95204fde46
jessica <jessica.farias.rosado@gmail.com>
2020-07-28 13:22:55 -0500

    Fixing issues of linters.yml

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,20 +4,20 @@ require './lib/messages'
 describe Jokes do
   describe '#jockes' do
     it 'request joke is a string' do
-      expect(Jokes.request_joke.class).to eql("string".class)
+      expect(Jokes.request_joke.class).to eql(String)
     end
     it 'initialize testing' do
-        var = DailyQuotes.new(1)
-        expect(var.id).to eql(1)
+      var = DailyQuotes.new(1)
+      expect(var.id).to eql(1)
     end
   end
   describe '#dailyquotes' do
     it 'request quote is a string' do
-        expect(DailyQuotes.request_quote.class).to eql("string".class)
+      expect(DailyQuotes.request_quote.class).to eql(String)
     end
     it 'initialize testing' do
-        var = DailyQuotes.new(1)
-        expect(var.id).to eql(1)
+      var = DailyQuotes.new(1)
+      expect(var.id).to eql(1)
     end
   end
 end
