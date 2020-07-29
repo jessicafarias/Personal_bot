0db0f698dd0563e374b1b8ba3614355c8c64f022
jessicafarias <jessica.farias.rosado@gmail.com>
2020-07-29 12:23:11 -0500

    Fix rspec errors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require './telegram/bot'
+require_relative '../lib/telegram/bot'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
